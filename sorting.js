bf53025867d57937a15912c10e3bf744d9cb71a6
Samuel <samuelrivba@gmail.com>
2024-04-07 19:12:24 -0500

    Add selectionSort implementation

--- sorting.js
+++ sorting.js
@@ -32,3 +32,20 @@ function bubbleSort(array) {
 }
 
 console.log(bubbleSort(numbers));
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    let temp = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
+
+console.log(selectionSort(numbers));
