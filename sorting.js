df6b0445f741609b39c9fc9c9340912ad69f918c
Samuel <samuelrivba@gmail.com>
2024-04-07 19:12:45 -0500

    Add insertionSort implementation

--- sorting.js
+++ sorting.js
@@ -49,3 +49,20 @@ function selectionSort(array) {
 }
 
 console.log(selectionSort(numbers));
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+  return array;
+}
+
+console.log(insertionSort(numbers));
